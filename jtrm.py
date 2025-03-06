60c72eaa15daed1390560becbfe4d45f65c254e0
igrtkv <igrtkv@yandex.ru>
2025-03-06 23:47:23 +0300

    наработки для работа с локальными шаблонами и файлами данных

--- jtrm.py
+++ jtrm.py
@@ -2,7 +2,11 @@
 
 import argparse
 import json
-from jinja2 import Environment, FileSystemLoader
+import os
+import codecs
+from jinja2 import Environment, FileSystemLoader, Template
+
+DEFAULT_TEMPLATES_PATH = "./templates"
 
 class JinjaTemplateReportMachine:
     DEFAULT_TEMPLATE = ""
@@ -25,20 +29,52 @@ class JinjaTemplateReportMachine:
         parser.add_argument("-o", "--output")
         return parser.parse_args()
 
-    def get_data(self):
-        # with open(self.data_file, "r") as f:
-        #     return json.loads(f)
-        return json.loads('{ "username" : "Igrtkv"} ')
 
-    def render(self):
-        env = Environment(loader=FileSystemLoader("./templates"))
-        template = env.get_template("test-template.md")
-        data = self.get_data()
-        print(template.render(data))
+    def render(self, data: dict, template: Template):
+        return template.render(data)
+
+
+class LocalReportGenerator:
+    def __init__(self,
+            template_file=DEFAULT_TEMPLATE, 
+            data_file=DEFAULT_DATA, 
+            output_file=DEFAULT_OUTPUT
+    ):
+        self._jtrm = JinjaTemplateReportMachine()
+        self._template_file = template_file
+        self._data_file = data_file
+        self._output_file = output_file
+
+    def _read_data(self):
+        if not os.path.exists(self._data_file):
+            return {}
+
+        with codecs.open(self._data_file, "r+", encoding="utf-8") as f:
+            return json.load(f)
+
+    def _read_template(self):
+        env = Environment(loader=FileSystemLoader("./"))
+        return env.get_template(self._template_file)
+
+    def generate_report(self):
+        template = self._read_template()
+        data = self._read_data()
+
+        output = self._jtrm.render(data, template)
+        with codecs.open(self._output_file, "w", encoding="utf-8") as of:
+            of.writelines(output)
+
+    def parse_arguments(self):
+        parser = argparse.ArgumentParser(prog="jtpm")
+        parser.add_argument("-t", "--template", required=True)
+        parser.add_argument("-d", "--data", required=True)
+        parser.add_argument("-o", "--output")
+        return parser.parse_args()
+
 
 if __name__ == "__main__":
     jtrm = JinjaTemplateReportMachine()
     # jtrm.parse_arguments()
-    jtrm.render()
+    jtrm.render2()
 
 
